be26c3f7ed1152fc71afd5b81d19fcd0fade46de
Reed Espinosa <reed.espinosa@nasa.gov>
2019-06-21 11:12:19 -0400

    adding function for simpsons rule integration

--- miscFunctions.py
+++ miscFunctions.py
@@ -20,3 +20,20 @@ def angstrmIntrp(lmbdIn, tau, lmbdTrgt):
 def angstrm(lmbd, tau):
     assert (lmbd.shape[0]==2 and tau.shape[0]==2), "Exactly two values must be provided!"
     return -np.log(tau[0]/tau[1])/np.log(lmbd[0]/lmbd[1])
+    
+def simpsonsRule(f,a,b,N=50):
+    """
+    simpsonsRule: (func, array, int, int) -> float
+    Parameters:
+		f: function that returns the evaluated equation at point x.
+        a, b: integers representing lower and upper bounds of integral.
+		N: integers number of segments being used to approximate the integral (same n as http://en.wikipedia.org/wiki/Simpson%27s_rule)
+	Returns float equal to the approximate integral of f(x) from bnds[0] to bnds[1] using Simpson's rule.
+    """
+    assert np.mod(N,2)==0, 'n must be even!'
+    dx = (b-a)/N
+    x = np.linspace(a,b,N+1)
+    y = f(x)
+    S = dx/3 * np.sum(y[0:-1:2] + 4*y[1::2] + y[2::2])
+    return S
+    
