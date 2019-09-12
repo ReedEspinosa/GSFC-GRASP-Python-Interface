996e0250a93f7ae1bddb8eae35287ef5d2f9a4ea
Reed Espinosa <reed.espinosa@nasa.gov>
2019-09-12 11:36:23 -0400

    final version used to generate the V1 dust LUT that were emailed out

--- generateLUT_PMelements.py
+++ generateLUT_PMelements.py
@@ -7,7 +7,7 @@ from netCDF4 import Dataset
 import sys
 sys.path.append("/Users/wrespino/Synced/Local_Code_MacBook/MADCAP_Analysis")
 from MADCAP_functions import loadVARSnetCDF
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 binPathGRASP = '/Users/wrespino/Synced/Local_Code_MacBook/grasp_open/build/bin/grasp' # currently has lambda checks disabled
 YAMLpath = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/newOpticsTables/LUT-DUST/settings_BCK_ExtSca_8lambda.yml'
@@ -39,7 +39,7 @@ thtv = np.zeros(len(msTyp))
 phi = np.zeros(len(msTyp)) 
 sza = 0
 
-loaddVarNames = ['lambda', 'qext', 'refreal', 'refimag']
+loaddVarNames = ['lambda', 'qext', 'qsca', 'refreal', 'refimag']
 optTbl = loadVARSnetCDF(netCDFpath, loaddVarNames)
 wvls = optTbl['lambda']*1e6
 gspRun = []
@@ -58,7 +58,7 @@ for bn in range(Nbin):
         n = optTbl['refreal'][bn,0,wvInds]
         n = np.minimum(n, nBnds[1])
         n = np.maximum(n, nBnds[0])
-        k = optTbl['refimag'][bn,0,wvInds]
+        k = -optTbl['refimag'][bn,0,wvInds]
         k = np.minimum(k, kBnds[1])
         k = np.maximum(k, kBnds[0])
         gspRunNow.YAMLmodify(lgnrmfld, szVars[bn])
@@ -67,7 +67,7 @@ for bn in range(Nbin):
         gspRunNow.addPix(nowPix)
         gspRun.append(gspRunNow)
 gDB = rg.graspDB(gspRun, maxCPU)
-rslts = gDB.processData(savePath=savePath_netCDF[0:-2]+'pkl', binPathGRASP=binPathGRASP)
+#rslts = gDB.processData(savePath=savePath_netCDF[0:-2]+'pkl', binPathGRASP=binPathGRASP)
 
 root_grp = Dataset(savePath_netCDF, 'w', format='NETCDF4')
 #root_grp = Dataset('/Users/wrespino/Desktop/netCDF_TEST.nc', 'w', format='NETCDF4')
@@ -75,14 +75,14 @@ root_grp.description = 'Single scattering properties of dust bins derived with G
 
 # dimensions
 root_grp.createDimension('sizeBin', Nbin)
-root_grp.createDimension('wavelength', Nlambda)
+root_grp.createDimension('lambda', Nlambda)
 root_grp.createDimension('angle', Nangles)
 
 # variables
 sizeBin = root_grp.createVariable('sizeBin', 'u1', ('sizeBin'))
 sizeBin.units = 'none'
 sizeBin.long_name = 'dust size bin index'
-wavelength = root_grp.createVariable('lambda', 'f4', ('wavelength'))
+wavelength = root_grp.createVariable('lambda', 'f4', ('lambda'))
 wavelength.units = 'um'
 wavelength.long_name = 'wavelength'
 angle = root_grp.createVariable('angle', 'f4', ('angle'))
@@ -91,21 +91,21 @@ angle.long_name = 'scattering angle'
 PMvarNms = ['p11', 'p12', 'p22', 'p33', 'p34', 'p44']
 PMvars = dict()
 for varNm in PMvarNms:
-    PMvars[varNm] = root_grp.createVariable(varNm, 'f8', ('sizeBin', 'wavelength', 'angle'))
+    PMvars[varNm] = root_grp.createVariable(varNm, 'f8', ('sizeBin', 'lambda', 'angle'))
     PMvars[varNm].units = 'sr-1'
-    PMvars[varNm].long_name = varNm + 'phase matrix element' 
-bext = root_grp.createVariable('bext', 'f8', ('sizeBin', 'wavelength'))
+    PMvars[varNm].long_name = varNm + ' phase matrix element' 
+bext = root_grp.createVariable('bext', 'f8', ('sizeBin', 'lambda'))
 bext.units = 'm-1'
 bext.long_name = 'extinction coefficient'
-bsca = root_grp.createVariable('bsca', 'f8', ('sizeBin', 'wavelength'))
+bsca = root_grp.createVariable('bsca', 'f8', ('sizeBin', 'lambda'))
 bsca.units = 'm-1'
 bsca.long_name = 'scattering coefficient'
-refreal = root_grp.createVariable('refreal', 'f8', ('sizeBin', 'wavelength'))
+refreal = root_grp.createVariable('refreal', 'f8', ('sizeBin', 'lambda'))
 refreal.units = 'none'
 refreal.long_name = 'real refractive index'
-reafimag = root_grp.createVariable('reafimag', 'f8', ('sizeBin', 'wavelength'))
-refreal.units = 'none'
-refreal.long_name = 'imaginary refractive index'
+reafimag = root_grp.createVariable('reafimag', 'f8', ('sizeBin', 'lambda'))
+reafimag.units = 'none'
+reafimag.long_name = 'imaginary refractive index'
 rv = root_grp.createVariable('rv', 'f8', ('sizeBin'))
 rv.units = 'um'
 rv.long_name = 'lognormal volume median radius'
@@ -138,7 +138,6 @@ for i,rslt in enumerate(rslts):
     reafimag[binInd, lInd] = rslt['k']
     for varNm in PMvarNms:
         for i,l in enumerate(lInd):
-#            print('%s>>> bin: %d - lInd: %d - rv[rslt]: %4.2f um - lambda[rslt]: %5.3f um' % (varNm, binInd, l, rslt['rv'][0], rslt['lambda'][i]))
             PMvars[varNm][binInd, l, :] = rslt[varNm][:,0,i]
 #            if varNm == 'p11' and binInd==4:
 #                if l%4 == 0:
@@ -150,3 +149,26 @@ for i,rslt in enumerate(rslts):
 #                plt.plot(np.r_[0:Nangles], rslt[varNm][:,0,i])
 #                lgTxt.append(str(rslt['lambda'][i]))
 root_grp.close()
+
+
+# SANITY CHECK PLOT
+loaddVarNames = ['lambda', 'bext', 'bsca']
+optTblNew = loadVARSnetCDF(savePath_netCDF, loaddVarNames)
+fitInd = np.r_[2,4,5,6,7,8,9,11,13]
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3.5))
+Vars = ['ext', 'sca']
+for i, vnm in enumerate(Vars):
+    vnmq = 'q'+vnm
+    vnmb = 'b'+vnm
+    ax[i].plot(optTbl['lambda']*1e6, optTbl['q'+vnm][:,0,:].T, '--')
+    ax[i].set_prop_cycle(None)
+    Scl = optTblNew['b'+vnm][:,8]/optTbl['q'+vnm][:,0,8]
+    ax[i].plot(optTblNew['lambda'], optTblNew['b'+vnm].T/Scl)
+    ax[i].set_xlim([0.3, 3.0])
+    ax[i].set_xlabel('wavelength')   
+    ax[i].set_ylabel('$q_{' + vnm + '}$')
+ax[0].legend(['Mode %d' % int(x+1) for x in range(optTblNew['b'+vnm].shape[0])])
+ax[0].set_prop_cycle(None)
+ax[0].plot(optTblNew['lambda'][fitInd], np.atleast_2d(optTbl['qext'][:,0,fitInd]).T, 'x')
+plt.suptitle('Orignal (dashed), target point (crosses), and new (solid) spectral dependences', fontsize=12)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
