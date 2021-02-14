17c00cf5fd34a8555e78b1b92bd07f242fdb1d89
Reed Espinosa <reed.espinosa@nasa.gov>
2021-02-14 14:32:21 -0500

    adding AOD plot to simulation quicklook

--- simulationQuickLook.py
+++ simulationQuickLook.py
@@ -8,7 +8,7 @@ from simulateRetrieval import simulation
 from glob import glob
 
 waveInd = 2
-fnPtrn = 'Test39_modis_HuamboVegetation_tFct1.000_sza*_phi*_n0_nAng*.pkl'
+fnPtrn = 'Test52_modis_HuamboVegetation_tFct1.000_sza*_phi*_n0_nAng*.pkl'
 inDirPath = '/Users/wrespino/synced/Working/TASNPP_simulation00/'
 saveFN = 'MERGED_'+fnPtrn.replace('*','ALL')
 files = glob(os.path.join(inDirPath, fnPtrn))
@@ -43,22 +43,43 @@ pprint(sim.analyzeSim(waveInd)[0])
 # 
 
 FS = 12
-plt.figure()
-true = np.asarray([rf['ssa'][waveInd] for rf in sim.rsltFwd])
-rtrv = np.asarray([rf['ssa'][waveInd] for rf in sim.rsltBck])
+fig, ax = plt.subplots(1,2, figsize=(10,5))
+true = np.asarray([rf['aod'][waveInd] for rf in sim.rsltFwd])
+rtrv = np.asarray([rf['aod'][waveInd] for rf in sim.rsltBck])
 minSSA = np.min(true)*0.95
-plt.plot(true, rtrv, '.')
-plt.plot([minSSA,1], [minSSA,1], 'k')
-plt.xlim(minSSA,1)
-plt.ylim(minSSA,1)
+ax[0].plot(true, rtrv, '.')
+ax[0].plot([minSSA,1], [minSSA,1], 'k')
+ax[0].set_xlim(minSSA,1)
+ax[0].set_ylim(minSSA,1)
+ax[0].set_title('AOD')
+ax[0].set_xlabel('True')
+ax[0].set_ylabel('Retrieved')
 Rcoef = np.corrcoef(true, rtrv)[0,1]
 RMSE = np.sqrt(np.median((true - rtrv)**2))
 bias = np.mean((rtrv-true))
 textstr = 'N=%d\nR=%.3f\nRMS=%.3f\nbias=%.3f' % (len(true), Rcoef, RMSE, bias)
-tHnd = plt.annotate(textstr, xy=(0, 1), xytext=(4.5, -4.5), va='top', xycoords='axes fraction',
+tHnd = ax[0].annotate(textstr, xy=(0, 1), xytext=(4.5, -4.5), va='top', xycoords='axes fraction',
                     textcoords='offset points', color='k', fontsize=FS)
 tHnd.set_bbox(dict(facecolor='white', alpha=0.7, edgecolor='None'))
 
+true = np.asarray([rf['ssa'][waveInd] for rf in sim.rsltFwd])
+rtrv = np.asarray([rf['ssa'][waveInd] for rf in sim.rsltBck])
+minSSA = np.min(true)*0.95
+ax[1].plot(true, rtrv, '.')
+ax[1].plot([minSSA,1], [minSSA,1], 'k')
+ax[1].set_xlim(minSSA,1)
+ax[1].set_ylim(minSSA,1)
+ax[1].set_title('SSA')
+ax[1].set_ylabel('Retrieved')
+Rcoef = np.corrcoef(true, rtrv)[0,1]
+RMSE = np.sqrt(np.mean((true - rtrv)**2))
+bias = np.mean((rtrv-true))
+textstr = 'N=%d\nR=%.3f\nRMS=%.3f\nbias=%.3f' % (len(true), Rcoef, RMSE, bias)
+tHnd = ax[1].annotate(textstr, xy=(0, 1), xytext=(4.5, -4.5), va='top', xycoords='axes fraction',
+                    textcoords='offset points', color='k', fontsize=FS)
+tHnd.set_bbox(dict(facecolor='white', alpha=0.7, edgecolor='None'))
+
+plt.tight_layout()
 plt.ion()
 plt.show()
 
