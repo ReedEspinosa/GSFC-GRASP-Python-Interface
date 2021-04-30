2e65dcf858b000ae5d6fdf6ab158d2d6c80e6ddb
Reed Espinosa <reed.espinosa@nasa.gov>
2021-04-30 18:50:05 -0400

    finishing runRetrievalOSSE.py example script

--- Examples/runRetrievalOSSE.py
+++ Examples/runRetrievalOSSE.py
@@ -23,7 +23,6 @@ from readOSSEnetCDF import osseData
 # import returnPixel and addError functions with instrument definitions from ...MADCAP_scripts/ACCP_ArchitectureAndCanonicalCases/architectureMap.py
 from architectureMap import returnPixel, addError
 
-from runGRASP import graspYAML
 
 # define other paths not having to do with the python code itself
 basePath = os.environ['NOBACKUP']
@@ -32,7 +31,7 @@ dirGRASP = os.path.join(basePath, 'grasp_open/build/bin/grasp') # location of th
 krnlPath = os.path.join(basePath, 'local/share/grasp/kernels') # location of GRASP kernel files
 osseDataPath = '/discover/nobackup/projects/gmao/osse2/pub/c1440_NR/OBS/A-CCP/' # base path for A-CCP OSSE data (contains gpm and ss450 folders)
 
-# randomize initial guess in YAML file before retrieving
+# if retrievals are divided up into multiple calls to GRASP, ensure the number of simultaneous processes is always ≤maxCPU
 maxCPU = 2
 
 # randomize initial guess in YAML file before retrieving
@@ -50,21 +49,19 @@ hour = 0
 # simulated orbit to use – gpm OR ss450
 orbit = 'ss450'
 
-# filter 
+# filter out pixels with mean SZA above this value (degrees)
 maxSZA = 90
 
+# true to skip retrievals on land pixels
 oceanOnly = False
-
-# name of instrument as defined in MADCAP_Scripts/ACCP_ArchitectureAndCanonicalCases/architectureMap.py
-archName = 'polar07'
 
 # If true no noise will be added to simulated measurements, else noise is added according to architectureMap.py (or Ed's simulations for lidar data)
 noiseFree = True 
 
-# general version tag to distinguish runs
+# general version integer to distinguish output files of different runs
 vrsn = 100
 
-# wavelengths (μm); if we only want specific λ set it here, otherwise use all netCDF files found
+# wavelengths (μm); if we only want specific λ set it here, otherwise use every λ found in the netCDF files
 wvls = [0.355, 0.36, 0.38, 0.41, 0.532, 0.55, 0.67, 0.87, 1.064, 1.55, 1.65] 
 
 # specific pixels to run; set to None to run all pixels (likely very slow) 
@@ -74,28 +71,28 @@ pixInd = [7375, 1444, 1359, 929, 4654, 6574, 2786, 6461, 6897, 2010] # SS Aug 20
 # save output here instead of within osseDataPath (None to disable)
 customOutDir = os.path.join(basePath, 'synced', 'Working', 'OSSE_Test_Run') 
 
-# create and instance of osseData with pixels from the specified date/time and extract the forward calculations in GRASP_scripts rslts dictionary format
+# create osseData instance w/ pixels from specified date/time (detail on these arguments in comment near top of osseData class's __init__ near readOSSEnetCDF.py:30)
 od = osseData(osseDataPath, orbit, year, month, day, hour, random=random, wvls=wvls, 
-              lidarVersion=None, maxSZA=maxSZA, oceanOnly=oceanOnly, loadDust=False, verbose=True)
+              lidarVersion=None, maxSZA=maxSZA, oceanOnly=oceanOnly, loadPSD=False, verbose=True)
+# extract the simulated observations and pack them in GRASP_scripts rslts dictionary format
 fwdData = od.osse2graspRslts(pixInd=pixInd, newLayers=None)
 
 # build file name to save the results
-savePath = od.fpDict['savePath'] % (vrsn, 'example', archName)
+savePath = od.fpDict['savePath'] % (vrsn, 'example', 'polarimeter07')
 if customOutDir: savePath = os.path.join(customOutDir, os.path.basename(savePath))
 print('-- Running simulation for ' + os.path.basename(savePath) + ' --') # we print this because savePath has useful information about OSSE data
 
-# NEEDED???
-yamlObj = graspYAML(bckYAMLpath, newTmpFile=('BCK_n%d' % nn))
-
-# Pull noise model for instrument defined above (if noiseFree==False)
+# Set noise model to added to polarimeter measurements, polar07 error is defined in addError() method of architectureMap.py
 radNoiseFun = None if noiseFree else functools.partial(addError, 'polar07')
 
-# define a new instance corresponding to this architecture and run the retrievals
-simA = rs.simulation() 
-simA.runSim(fwdData, yamlObj, maxCPU=maxCPU, maxT=20, savePath=savePath, 
+# define a new instance of the simulation class
+simA = rs.simulation()
+
+# run the retrievals (these arguments are explained at the top of the runSim method definition near simulateRetrieval.py:35)
+simA.runSim(fwdData, bckYAMLpath, maxCPU=maxCPU, maxT=20, savePath=savePath, 
             binPathGRASP=dirGRASP, intrnlFileGRASP=krnlPath, releaseYAML=True, lightSave=True, 
             rndIntialGuess=rndIntialGuess, radianceNoiseFun=radNoiseFun,
-            workingFileSave=True, dryRun=True, verbose=True)
+            workingFileSave=True, dryRun=False, verbose=True)
 
 
 
